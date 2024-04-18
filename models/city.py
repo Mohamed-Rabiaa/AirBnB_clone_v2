312f0dda68dd56dff64ef95f3f7fab7b18a73840
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-04-19 00:24:13 +0200

    task 8 and 9 and updated task 7

--- models/city.py
+++ models/city.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.state import State
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """ This is the class for City Attributes:
+        __tablename__: name of the table represented
+        state_id: The state id
+        name: input name
+    """
+
     __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey(State.id), nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
+    places = relationship("Place", cascade="all, delete", backref="city")
--- models/review.py
+++ models/review.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy.orm import relationship
-import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
-
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """This is the class for Review
+    Attributes:
+        __tablename__: table name
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
     # user = relationship("User", back_populates="reviews", cascade="delete")
