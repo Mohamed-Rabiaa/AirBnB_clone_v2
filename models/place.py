c22f0a92917af02b77de85a1db01e1f79378c481
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-04-25 10:49:50 -0700

    Updating Amenity & Place classes

--- models/place.py
+++ models/place.py
@@ -3,17 +3,25 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, Table, ForeignKey
 from sqlalchemy.orm import relationship
-import models
 from os import getenv
 import models
 
-'''
+
 place_amenity = Table(
-    'place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey("places.id")),
-    Column('amenity_id', String(60), ForeignKey("amenities.id"))
-)
-'''
+    'place_amenity',
+    Base.metadata,
+    Column(
+        'place_id',
+        String(60),
+        ForeignKey("places.id"),
+        primary_key=True,
+        nullable=False),
+    Column(
+        'amenity_id',
+        String(60),
+        ForeignKey("amenities.id"),
+        primary_key=True,
+        nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -38,6 +46,11 @@ class Place(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", back_populates="place",
                                cascade="all, delete-orphan")
+        amenities = relationship(
+            "Amenity",
+            secondary=place_amenity,
+            back_populates="place_amenities",
+            viewonly=False)
 
     else:
         @property
@@ -52,27 +65,21 @@ class Place(BaseModel, Base):
                 v for k, v in models.storage.all(models.Review).items()
                 if v.place_id == self.id
             ]
-            return (list)
+            return list
 
-    '''
-   __amenities = relationship(
-        "Amenity",
-        secondary=place_amenity,
-        backref="place",
-        viewonly=False
-    )
-    '''
+        @property
+        def amenities(self):
+            """get all amenities with the current place id
+            """
+            list = [
+                v for k, v in models.storage.all(models.Amenity).items()
+                if v.id in self.amenity_ids
+            ]
+            return list
 
-    '''
-    @property
-    def amenities(self):
-        """get all amenities with the current place id
-        """
-        if env.get('HBNB_TYPE_STORAGE') == 'db':
-            return self.__amenities
-        list = [
-            v for k, v in models.storage.all(models.Amenity).items()
-            if v.id in self.amenity_ids
-        ]
-        return (list)
-    '''
+        @amenities.setter
+        def amenities(self, obj):
+            """ handles append method for adding an Amenity.id to
+            the attribute amenity_ids """
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
