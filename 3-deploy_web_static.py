01aa67539394ab63d0d6903336680396a05fdd93
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-05-05 11:12:17 -0700

    Fixing 3-deploy_web_static.py script

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,8 +4,14 @@ This script creates and distributes an archive to your web servers,
 using the function deploy
 """
 
+from fabric.api import env
+
+
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+env.hosts = ['52.90.0.3', '54.210.51.202']
+env.user = 'ubuntu'
 
 
 def deploy():
