e78e3543fac4eb9102640e9629f8a7149de250bd
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-05-16 22:01:19 -0700

    Adding web_flask scripts & templates

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,5 +14,11 @@ def home():
     return "Hello HBNB!"
 
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays HBNB on the hbnb page"""
+    return "HBNB"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,5 +14,17 @@ def home():
     return "Hello HBNB!"
 
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays HBNB on the hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    """Displays 'C' followed by the value of the text variable """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
