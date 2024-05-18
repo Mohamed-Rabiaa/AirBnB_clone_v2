a19a6e11ce3181f4381134784ecfb4d32314f227
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-05-18 16:14:08 -0700

    Adding 8-cities_by_states.py script & 9-states.html template

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" This script starts a Flask web application """
+
+
+from models import storage
+from models.state import State
+from models.city import City
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states_list():
+    """
+    Displays an HTML page that contains a list of
+    all State objects present in DBStorage sorted by name
+    """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def removesession(self):
+    """
+    Removes the current SQLAlchemy Session after
+    each request
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
