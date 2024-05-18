5de6364f132716b791be10554baa20d3253354b3
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-05-18 11:35:19 -0700

    Fixing code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,18 +5,7 @@
 from models import storage
 from models.state import State
 from flask import Flask, render_template
-
-
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def removesession():
-    """
-    Removes the current SQLAlchemy Session after
-    each request
-    """
-    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -26,7 +15,17 @@ def states_list():
     all State objects present in DBStorage sorted by name
     """
     obj_dct = storage.all(State)
-    return render_template('7-states_list.html', obj_dct)
+    states = obj_dct.values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def removesession(self):
+    """
+    Removes the current SQLAlchemy Session after
+    each request
+    """
+    storage.close()
 
 
 if __name__ == '__main__':
